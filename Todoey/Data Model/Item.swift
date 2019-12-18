a7ab7f7e345b7e577d9c50440f120e544ed99689
onevoice <onevoice@gmail.com>
2019-12-18 16:26:11 +0900

    Convert to MVC Pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by 崔仁燮 on 2019/12/18.
+//  Copyright © 2019 Bryan CHOI. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
